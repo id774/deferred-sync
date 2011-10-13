2abf8c84c5e1f683611d1d89a0df91561088d4e7
id774 <idnanashi@gmail.com>
2011-10-13 21:57:01 +0900

    1 first commit

---
+++ bin/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+setup() {
+    dir_name() { echo ${1%/*}; }
+    SCRIPT_HOME=$(cd $(dir_name $0) && pwd)
+    . $SCRIPT_HOME/../config/backup.conf
+    EXECDIR=${0%/*}
+    DATE=`date +%Y%m%d`
+}
+
+call_func() {
+    while [ $# -gt 0 ]
+    do
+        . $SCRIPT_HOME/../lib/$1.sh
+        shift
+    done
+}
+
+main() {
+    setup
+    call_func \
+      get_resources \
+      dump_svn \
+      incremental_backup \
+      backup_to_remote
+}
+
+main
