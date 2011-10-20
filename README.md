16c57cbf0982a87d687b41c02a0252b9bf7f1634
id774 <idnanashi@gmail.com>
2011-10-20 17:45:31 +0900

    24 Added the installer

--- README.md
+++ README.md
@@ -17,10 +17,5 @@ Author
 Copyright and license
 ---------------------
 
-This software licensed under Dual-Licensing GPL and other FLOSS licenses.
-See LICENSE file.
-
-the GNU GENERAL PUBLIC LICENSE, Version 3.0.
-+ http://www.gnu.org/copyleft/gpl.html
-
+See the file doc/COPYING (.ja).
 
---
+++ installer/installer.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+setup() {
+    dir_name() { echo ${1%/*}; }
+    SCRIPT_HOME=$(cd $(dir_name $0) && pwd)/..
+    EXECDIR=${0%/*}
+
+    case $OSTYPE in
+      *darwin*)
+        OPTIONS=-Rv
+        OWNER=root:wheel
+        ;;
+      *)
+        OPTIONS=-Rvd
+        OWNER=root:root
+        ;;
+    esac
+
+    test -n "$1" && TARGET=$1
+    test -n "$1" || TARGET=/opt/deferred-sync
+    test -n "$2" || SUDO=sudo
+    test -n "$2" && SUDO=
+}
+
+deploy() {
+    while [ $# -gt 0 ]
+    do
+        $SUDO cp $OPTIONS $SCRIPT_HOME/$1/ $TARGET/
+        shift
+    done
+}
+
+
+deploy_to_target() {
+    echo "Installing from $SCRIPT_HOME to $TARGET/"
+    test -d $TARGET && $SUDO rm -rf $TARGET/
+    test -d $TARGET || $SUDO mkdir -p $TARGET/
+    deploy bin config etc lib
+}
+
+set_permission() {
+    $SUDO chown -R $OWNER $TARGET
+}
+
+installer() {
+    setup $*
+    deploy_to_target
+    test -n "$2" || set_permission
+}
+
+installer $*
