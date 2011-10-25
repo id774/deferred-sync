1f94ad6b500d7bce9900f6cba2e7720b01671839
id774 <idnanashi@gmail.com>
2011-10-25 11:11:03 +0900

    28 Fix bug of dump_mysql

--- lib/30_dump_mysql.sh
+++ lib/30_dump_mysql.sh
@@ -5,9 +5,9 @@ get_mysqldump() {
     test -f $MYSQLDUMP/$1.zip && \
       rm $MYSQLDUMP/$1.zip
     mysqldump --add-drop-table --add-locks --password=$3 -u $2 \
-        $1 > $MYSQLDUMP/mysqldump/$1.sql \
-        && zip $MYSQLDUMP/mysqldump/$1.zip $MYSQLDUMP/mysqldump/$1.sql \
-        && rm $MYSQLDUMP/mysqldump/$1.sql
+        $1 > $MYSQLDUMP/$1.sql \
+        && zip $MYSQLDUMP/$1.zip $MYSQLDUMP/$1.sql \
+        && rm $MYSQLDUMP/$1.sql
     echo "Return code is $?"
 }
 
