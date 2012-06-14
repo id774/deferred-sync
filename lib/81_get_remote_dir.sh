26a8269140b92c2a7526e77216950aa6862eb539
id774 <idnanashi@gmail.com>
2012-06-14 15:51:26 +0900

    50 Get the multiple remote hosts

--- lib/81_get_remote_dir.sh
+++ lib/81_get_remote_dir.sh
@@ -1,18 +1,26 @@
 # lib/get_remote_dir.sh
 
-get_remote_dir() {
-    echo -n "* Executing get remote dir on "
-    date "+%Y/%m/%d %T"
+get_remote() {
     OPTS="-avz --delete -e ssh"
     if [ "$DRY_RUN" = "true" ]; then
       OPTS="$OPTS --dry-run"
     fi
     if [ -d $GET_TARGET_DIR ]; then
         echo "rsync $OPTS root@$GET_HOST:$GET_REMOTE_DIR $GET_TARGET_DIR"
-        ping -c 1 $GET_HOST > /dev/null 2>&1 && \
-          rsync $OPTS root@$GET_HOST:$GET_REMOTE_DIR $GET_TARGET_DIR
+        ping -c 1 $1 > /dev/null 2>&1 && \
+          rsync $OPTS root@$GET_HOST:$GET_REMOTE_DIR $GET_TARGET_DIR/$1
         echo "Return code is $?"
     fi
+}
+
+get_remote_dir() {
+    echo -n "* Executing get remote dir on "
+    date "+%Y/%m/%d %T"
+    for GET_HOST in $GET_HOSTS
+    do
+        get_remote $GET_HOST
+    done
+    unset REMOTE_HOST
 }
 
 echo "- module get_remote_dir loaded"
